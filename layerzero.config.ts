2c899ebf65fa7ebb50979f9c4b96dc5240300f79
ANDREA NISTICO <nistic.andrea@gmail.com>
2025-09-09 12:47:15 +0200

    set 3 confirmations

--- layerzero.config.ts
+++ layerzero.config.ts
@@ -53,7 +53,7 @@ const pathways: TwoWayConfig[] = [
         baseContract, // Chain A contract
         bscContract, // Chain B contract
         [['Horizen'], [['LayerZero Labs', 'Nethermind'], 1]], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
-        [1, 1], // [A to B confirmations, B to A confirmations]
+        [3, 3], // [A to B confirmations, B to A confirmations]
         [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain B enforcedOptions, Chain A enforcedOptions
     ],
 ]

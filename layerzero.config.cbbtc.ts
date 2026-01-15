2ee3e0ca65e5d8a69fff1e66564652f4569bb5ef
ANDREA NISTICO <nistic.andrea@gmail.com>
2026-01-15 22:59:34 +0100

    env and configs

--- layerzero.config.cbbtc.ts
+++ layerzero.config.cbbtc.ts
@@ -18,26 +18,26 @@ const zenContract: OmniPointHardhat = {
 
 //ONLY FOR MAINNET GAS PROFILING -> first config; then test; change numbrs; repeat config
 
-const lzReceiveGasProfilingA = 110_000; //read this on horizen chain (_lzReceive) after a test transfer from Base to Horizen -> 30% more
+const lzReceiveGasProfiling = 110_000; //read this on horizen chain (_lzReceive) after a test transfer from Base to Horizen -> 30% more
 
 const EVM_ENFORCED_OPTIONS_A_TO_B: OAppEnforcedOption[] = [
   {
     msgType: 1,
     optionType: ExecutorOptionType.LZ_RECEIVE,
-    gas: lzReceiveGasProfilingA,
+    gas: lzReceiveGasProfiling,
     value: 0,
   },
   {
     msgType: 2,
     optionType: ExecutorOptionType.LZ_RECEIVE,
-    gas: lzReceiveGasProfilingA,
+    gas: lzReceiveGasProfiling,
     value: 0,
   },
   {
     msgType: 2,
     optionType: ExecutorOptionType.COMPOSE,
     index: 0,
-    gas: lzReceiveGasProfilingA,
+    gas: lzReceiveGasProfiling,
     value: 0,
   },
 ]
@@ -56,7 +56,22 @@ export default async function () {
     // Generate the connections config based on the pathways
     const connections = await generateConnectionsConfig(pathways)
     return {
-        contracts: [{ contract: baseContract }, { contract: zenContract }],
+        contracts: [
+            { 
+                contract: baseContract,
+                config: {
+                    delegate: process.env.BASE_DELEGATE_ADDRESS || '',
+                    owner: process.env.BASE_OWNER_ADDRESS || '',
+                },
+            },
+            { 
+                contract: zenContract,
+                config: {
+                    delegate: process.env.HORIZEN_DELEGATE_ADDRESS || '',
+                    owner: process.env.HORIZEN_OWNER_ADDRESS || '',
+                },
+            }
+        ],
         connections,
     }
 }
